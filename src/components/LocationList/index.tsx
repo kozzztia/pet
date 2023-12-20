a8ad934f05656496b50f1a5a2a9ba5efd3b38eff
kostia <kozzztia@ukr.net>
2023-12-20 18:54:41 +0200

    useSWR

---
+++ src/components/LocationList/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import {FlatList} from 'react-native-gesture-handler';
+
+const List = () => {
+  return <FlatList data={} renderItem={} keyExtractor={}/>;
+};
+
+export default List;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, {useEffect, useLayoutEffect} from 'react';
 import {Layout} from '../layouts';
 import {ScreenNavigationProp} from '../types/type';
 import {CustomButton} from '../components/ui/Buttons';
+import useSWR from 'swr';
+import {Text} from 'react-native';
 
 type HomeScreenProps = {
   navigation: ScreenNavigationProp;
 };
 
+type Location = {
+  created: string;
+  dimension: string;
+  id: number;
+  name: string;
+  residents: string[]; // Assuming an array of strings for residents, you can adjust this based on the actual structure
+  type: string;
+  url: string;
+};
+
+const fetcher = (url: string | URL | Request) =>
+  fetch(url)
+    .then(res => res.json())
+    .then(data => data.results);
+
 const HomeScreen: React.FC<HomeScreenProps> = ({navigation}) => {
+  const api = 'https://rickandmortyapi.com/api/location';
+  const {data, error} = useSWR(api, fetcher);
+  if (error) {
+    return <Text>Error loading data</Text>;
+  }
+
+  if (!data) {
+    return <Text>Loading...</Text>;
+  }
   return (
     <Layout>
-      <CustomButton
+      {/* <CustomButton
         handler={() => navigation.navigate('Game')}
         title={'Go to Game'}
-      />
+      /> */}
+      {data.map((item: Location) => (
+        <Text key={item.id}>{item.name}</Text>
+      ))}
     </Layout>
   );
 };
