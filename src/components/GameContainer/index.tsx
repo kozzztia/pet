de1be7d817f1bafaaf483d1fe3ce4b6377a4751e
kostia <kozzztia@ukr.net>
2024-01-20 13:14:54 +0200

    rundom 16 game|Residents returned

--- src/components/GameContainer/index.tsx
+++ src/components/GameContainer/index.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
 import {ScrollView, View, StyleSheet, Image} from 'react-native';
 import {GameResident} from '../../types/residentType';
+import {SIZES} from '../../styles';
 
 interface GameContainerProps {
   gameResidents: GameResident[];
 }
 
 const GameContainer: React.FC<GameContainerProps> = ({gameResidents}) => {
-  console.log(gameResidents);
   return (
     <ScrollView contentContainerStyle={[styles.container]}>
       <View style={styles.cardsContainer}>
         {gameResidents?.map(item => (
           <Image
-            style={{width: 100, height: 100}}
+            key={item.id}
+            style={{width: 90, height: 90, margin: SIZES.mainMargin}}
             source={{uri: item?.image as string}}
           />
         ))}
--- src/utils/gameUtils/index.tsx
+++ src/utils/gameUtils/index.tsx
@@ -1,32 +1,67 @@
+import React from 'react';
 import {GameResident, Resident} from '../../types/residentType';
 
-// const getRandomArray = (length: number): number[] => {
-//   const array: number[] = [];
-//   while (array.length < length) {
-//     const randomNumber: number = Math.floor(Math.random() * length);
-//     if (!array.includes(randomNumber)) {
-//       array.push(randomNumber);
-//     }
-//   }
-//   return array;
-// };
+const getRandomArray = (length: number): number[] => {
+  const array: number[] = [];
+  while (array.length < length) {
+    const randomNumber: number = Math.floor(Math.random() * length);
+    if (!array.includes(randomNumber)) {
+      array.push(randomNumber);
+    }
+  }
+  return array;
+};
 
-// const blended = (array: GameResident[], position: number[]): GameResident[] => {
-//   let result: GameResident[] = [];
-//   for (let i = 0; i < position.length; i++) {
-//     result.push(array[position[i]]);
-//   }
-//   return result;
-// };
+const blended = (array: Resident[]): Resident[] => {
+  const result: Resident[] = [];
+  const position = getRandomArray(array.length);
+  if (array.length < 8) {
+    for (let i = 0; i < array.length; i++) {
+      result.push(array[position[i]]);
+    }
+  }
+  if (array.length >= 8) {
+    for (let i = 0; i < 8; i++) {
+      result.push(array[position[i]]);
+    }
+  }
+  return result;
+};
+
+const resultBlend = (array: GameResident[]) => {
+  const result: GameResident[] = [];
+  const position = getRandomArray(array.length);
+  for (let i = 0; i < array.length; i++) {
+    result.push(array[position[i]]);
+  }
+  return result;
+};
 
 const blendedResidents = (residents: Resident[]): GameResident[] => {
   let result: GameResident[] = [];
-  if (!residents.length) {
-    result = [];
-  }
-  if (residents.length > 0) {
-    residents.forEach(item => {
+  let copyResult: GameResident[] = [];
+
+  const blendedArray = blended(residents);
+
+  if (blendedArray.length > 0 && residents.length < 8) {
+    blendedArray.forEach(item => {
       result.push({
+        name: item.name,
+        image: item.image,
+        id: `${item.id}`,
+        isOpen: false,
+      });
+    });
+  }
+  if (blendedArray.length >= 8) {
+    blendedArray.forEach(item => {
+      result.push({
+        name: item.name,
+        image: item.image,
+        id: `${item.id}-original`,
+        isOpen: false,
+      });
+      copyResult.push({
         name: item.name,
         image: item.image,
         id: `${item.id}-copy`,
@@ -34,6 +69,8 @@ const blendedResidents = (residents: Resident[]): GameResident[] => {
       });
     });
   }
-  return result;
+
+  return resultBlend([...result, ...copyResult]);
 };
+
 export {blendedResidents};
