a5571a36778fa5758fdf3e55cbc146869e674477
kostia <kozzztia@ukr.net>
2024-02-04 14:37:39 +0200

    try to get music

--- src/components/GameContainer/index.tsx
+++ src/components/GameContainer/index.tsx
@@ -3,12 +3,27 @@ import {View, StyleSheet} from 'react-native';
 import {GameResident} from '../../types/residentType';
 import CardGameContainer from './CardGameContainer';
 import BubleGameContainer from './BubleGameContainer';
+import Sound from 'react-native-sound';
+
+const backgroundMusic = new Sound(
+  '../../assets/music/back.mp3',
+  Sound.MAIN_BUNDLE,
+  (error?: Error) => {
+    if (error) {
+      console.log('Error loading sound: ', error);
+    } else {
+      console.log('Sound loaded successfully');
+    }
+  },
+);
 
 interface GameContainerProps {
   gameResidents: GameResident[];
 }
 
 const GameContainer: React.FC<GameContainerProps> = ({gameResidents}) => {
+  backgroundMusic.play();
+  backgroundMusic.setVolume(0.5);
   return (
     <View style={styles.container}>
       {gameResidents?.length < 8 ? (
